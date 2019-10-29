4980cc07ccf7c4548036750bc20f469160c22018
Oblum1989 <oscar.blum@gmail.com>
2019-10-28 23:46:08 -0500

    add event to button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value : 0
+    }
+    this.updateNumber = this.updateNumber.bind(this)
+  }
+  
+  updateNumber(){
+    this.setState({
+      value : this.state.value + 1
+    });
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.updateNumber}>Incrementa</button>
       </div>
     );
   }
